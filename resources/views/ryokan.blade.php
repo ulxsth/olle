d2815de602de48c4b5b7f3b2383193437ebff2f1
ulxsth <ulxsth@gmail.com>
2024-02-29 23:00:53 +0900

    upd: form追加

--- resources/views/ryokan.blade.php
+++ resources/views/ryokan.blade.php
@@ -1,6 +1,15 @@
 <html>
   <body>
-    ryokan
-    <a href="{{ route('onsen') }}">onsen</a>
+    <h1>STEP1</h1>
+    <form action="" method="post">
+      <label for="onsen">旅館</label>
+      <select name="onsen" id="onsen">
+        <option value="1">旅館1</option>
+        <option value="2">旅館2</option>
+        <option value="3">旅館3</option>
+      </select>
+
+      <button type="submit">next</button>
+    </form>
   </body>
 </html>
