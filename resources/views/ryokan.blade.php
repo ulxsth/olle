b11b6b4f220e0579e82ce2ef3c00ebdb94592e5c
ulxsth <ulxsth@gmail.com>
2024-03-01 00:13:18 +0900

    fix: 旅館のデータタグをryokanに修正

--- resources/views/ryokan.blade.php
+++ resources/views/ryokan.blade.php
@@ -3,8 +3,8 @@
     <h1>STEP1</h1>
     <form action="/ryokan" method="post">
       @csrf
-      <label for="onsen">旅館</label>
-      <select name="onsen" id="onsen">
+      <label for="ryokan">旅館</label>
+      <select name="ryokan" id="ryokan">
         <option value="1">旅館1</option>
         <option value="2">旅館2</option>
         <option value="3">旅館3</option>
