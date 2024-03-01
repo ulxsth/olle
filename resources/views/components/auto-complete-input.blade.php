63d705be412cfa00386378df5f495a163841155a
ulxsth <ulxsth@gmail.com>
2024-03-02 01:02:58 +0900

    upd: placeholderの分離ロジックをphpに分離

--- resources/views/components/auto-complete-input.blade.php
+++ resources/views/components/auto-complete-input.blade.php
@@ -1,3 +1,10 @@
+@php
+    $placeholder = "";
+    if ($type === 'ryokan') {
+        $placeholder = "旅館名を入力";
+    }
+@endphp
+
 @if ($type === 'ryokan')
-  <input id="pac-input" type="text" placeholder="旅館名を入力" />
+  <input id="pac-input" type="text" placeholder="{{ $placeholder }}" />
 @endif
