6cc87b3f6d335692cb442cecfe022d4fbb9a4887
ulxsth <ulxsth@gmail.com>
2024-03-02 00:58:32 +0900

    upd: /ryokanの入力フォームをコンポーネントに分離

---
+++ resources/views/components/auto-complete-input.blade.php
@@ -0,0 +1,3 @@
+@if ($type === 'ryokan')
+  <input id="pac-input" type="text" placeholder="旅館名を入力" />
+@endif
--- resources/views/ryokan.blade.php
+++ resources/views/ryokan.blade.php
@@ -1,15 +1,8 @@
-<html>
+<html lang="ja">
   <body>
-    <h1>STEP1</h1>
     <form action="/ryokan" method="post">
       @csrf
-      <label for="ryokan">旅館</label>
-      <select name="ryokan" id="ryokan">
-        <option value="旅館1">旅館1</option>
-        <option value="旅館2">旅館2</option>
-        <option value="旅館3">旅館3</option>
-      </select>
-
+      <x-auto-complete-input type="ryokan" />
       <button type="submit">next</button>
     </form>
   </body>
