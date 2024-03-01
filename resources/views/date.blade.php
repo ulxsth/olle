12b75c3eb4e77ce10ec5dfd674d3c80c1e3b662e
ulxsth <ulxsth@gmail.com>
2024-03-01 14:38:03 +0900

    upd: デフォルト値として現在の日付を入力しておくようにした

--- resources/views/date.blade.php
+++ resources/views/date.blade.php
@@ -5,7 +5,7 @@
     <form action="/date" method="post">
       @csrf
       <label for="date">日付</label>
-      <input type="date" name="date" id="date">
+      <input type="date" name="date" id="date" value="{{ date('Y-m-d') }}">
       <button type="submit">next</button>
     </form>
   </body>
