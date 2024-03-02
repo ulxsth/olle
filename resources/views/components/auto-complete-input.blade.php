ef594ce17c5e0d4b6a812a76c19bc1d8861006f1
ulxsth <ulxsth@gmail.com>
2024-03-02 17:06:26 +0900

    upd: /onsenの入力部をAutoCompleteInputに置換

--- resources/views/components/auto-complete-input.blade.php
+++ resources/views/components/auto-complete-input.blade.php
@@ -2,6 +2,8 @@
     $placeholder = "ここに入力";
     if ($type === 'ryokan') {
         $placeholder = "旅館名を入力";
+    } else if ($type === 'onsen') {
+        $placeholder = "温泉名を入力";
     }
 @endphp
 
--- resources/views/onsen.blade.php
+++ resources/views/onsen.blade.php
@@ -3,12 +3,7 @@
     <p>{{ session('ryokan') }}</p>
     <form action="/onsen" method="post">
       @csrf
-      <label for="onsen">温泉</label>
-      <select name="onsen" id="onsen">
-        <option value="温泉1">温泉1</option>
-        <option value="温泉2">温泉2</option>
-        <option value="温泉3">温泉3</option>
-      </select>
+      <x-auto-complete-input type="onsen" />
 
       <button type="submit">next</button>
     </form>
