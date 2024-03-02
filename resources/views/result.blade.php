5fa02b06953feac84388b5c677c9cff11cef1609
ulxsth <ulxsth@gmail.com>
2024-03-03 05:09:43 +0900

    fix: apyキーの前後の空白を削除

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,8 +1,15 @@
-<html>
-  <body>
-    <p>旅館: {{ session('ryokan') }}</p>
-    <p>温泉: {{ session('onsen') }}</p>
-    <p>日付: {{ session('date') }}</p>
-    <a href="{{ route('ryokan') }}">modoru</a>
-  </body>
-</html>
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <p>所要時間：<span id="duration"></span></p>
+        <div id="map"></div>
+    </div>
+@endsection
+
+@section('script')
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAP_API_KEY') }}&callback=initMap&libraries=places&v=weekly&solution_channel=GMP_CCS_autocomplete_v1"
+        defer></script>
+    <script src="{{ asset('js/result-map.js') }}"></script>
+@endsection
--- resources/views/ryokan.blade.php
+++ resources/views/ryokan.blade.php
@@ -10,7 +10,7 @@
 
 @section('script')
     <script
-        src="https://maps.googleapis.com/maps/api/js?key= {{ env('GOOGLE_MAP_API_KEY') }} &callback=initMap&libraries=places&v=weekly&solution_channel=GMP_CCS_autocomplete_v1"
+        src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAP_API_KEY') }}&callback=initMap&libraries=places&v=weekly&solution_channel=GMP_CCS_autocomplete_v1"
         defer></script>
     <script src="{{ asset('js/auto-complete.js') }}"></script>
 @endsection
