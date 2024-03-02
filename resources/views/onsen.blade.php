d26e45a7bbc32ec2beb961d23ecef0cf060f541d
ulxsth <ulxsth@gmail.com>
2024-03-03 04:57:52 +0900

    rename: auto-complete.js

--- resources/views/onsen.blade.php
+++ resources/views/onsen.blade.php
@@ -14,5 +14,5 @@
     <script
         src="https://maps.googleapis.com/maps/api/js?key= {{ env('GOOGLE_MAP_API_KEY') }} &callback=initMap&libraries=places&v=weekly&solution_channel=GMP_CCS_autocomplete_v1"
         defer></script>
-    <script src="{{ asset('js/google-map-api.js') }}"></script>
+    <script src="{{ asset('js/auto-complete.js') }}"></script>
 @endsection
