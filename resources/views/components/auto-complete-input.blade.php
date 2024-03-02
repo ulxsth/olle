8b71ce120b5dff0fb8f7e6cb1e7515c7b74af65e
ulxsth <ulxsth@gmail.com>
2024-03-02 17:04:00 +0900

    refactor: 不要なif分を削除

--- resources/views/components/auto-complete-input.blade.php
+++ resources/views/components/auto-complete-input.blade.php
@@ -1,10 +1,9 @@
 @php
-    $placeholder = "";
+    $placeholder = "ここに入力";
     if ($type === 'ryokan') {
         $placeholder = "旅館名を入力";
     }
 @endphp
 
-@if ($type === 'ryokan')
-  <input id="pac-input" type="text" placeholder="{{ $placeholder }}" />
-@endif
+<input id="pac-input" type="text" placeholder="{{ $placeholder }}" />
+
