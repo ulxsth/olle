4ba6a8dd0db6a59de62dac5fae10c4155628086a
ulxsth <ulxsth@gmail.com>
2024-03-03 03:50:04 +0900

    upd: スクリプト読み込み部分をscriptセクションに分離

--- resources/views/onsen.blade.php
+++ resources/views/onsen.blade.php
@@ -1,11 +1,18 @@
-@extends("app")
+@extends('app')
 
-@section("content")
-<p>{{ session('ryokan') }}</p>
-<form action="/onsen" method="post">
-  @csrf
-  <x-auto-complete-input type="onsen" placeholder="温泉名を入力" />
+@section('content')
+    <p>{{ session('ryokan') }}</p>
+    <form action="/onsen" method="post">
+        @csrf
+        <x-auto-complete-input type="onsen" placeholder="温泉名を入力" />
 
-  <button type="submit">next</button>
-</form>
+        <button type="submit">next</button>
+    </form>
+@endsection
+
+@section('script')
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key= {{ env('GOOGLE_MAP_API_KEY') }} &callback=initMap&libraries=places&v=weekly&solution_channel=GMP_CCS_autocomplete_v1"
+        defer></script>
+    <script src="{{ asset('js/google-map-api.js') }}"></script>
 @endsection
--- resources/views/ryokan.blade.php
+++ resources/views/ryokan.blade.php
@@ -1,9 +1,16 @@
 @extends('app')
 
 @section('content')
-<form action="/ryokan" method="post">
-    @csrf
-    <x-auto-complete-input type="ryokan" placeholder="旅館名を入力" />
-    <button type="submit">next</button>
-</form>
+    <form action="/ryokan" method="post">
+        @csrf
+        <x-auto-complete-input type="ryokan" placeholder="旅館名を入力" />
+        <button type="submit">next</button>
+    </form>
+@endsection
+
+@section('script')
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key= {{ env('GOOGLE_MAP_API_KEY') }} &callback=initMap&libraries=places&v=weekly&solution_channel=GMP_CCS_autocomplete_v1"
+        defer></script>
+    <script src="{{ asset('js/google-map-api.js') }}"></script>
 @endsection
