b90718b419d6f58b3907364f0288333ca83a9fd1
naoyuki <kamaboko912@gmail.com>
2024-03-03 23:48:57 +0900

    navも挿入に成功、header,navともにcssも適用

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,6 @@
+<nav class="nav">
+    <div></div>
+    <a href="#">
+        <div class="btn next__btn">次に進む</div>
+    </a>
+</nav>
--- resources/views/ryokan.blade.php
+++ resources/views/ryokan.blade.php
@@ -1,10 +1,11 @@
 @extends('app')
 
-@include('components.header')
 @section('content')
+@include('components.header')
 <form action="/ryokan" method="post">
     @csrf
     <x-auto-complete-input type="ryokan" placeholder="旅館名を入力" />
     <button type="submit">next</button>
 </form>
+@include('components.nav')
 @endsection
