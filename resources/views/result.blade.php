c2fd50593f8abe0fbcae2ab2b2291377581a70c4
ulxsth <ulxsth@gmail.com>
2024-03-04 02:14:22 +0900

    upd: html内に温泉、旅館のLatLng値を格納するように

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -4,8 +4,11 @@
     <div class="container">
         <p>所要時間：<span id="duration"></span></p>
         <div id="map" style="width: 100%; height: 100vh"></div>
-        <input type="hidden" id="start" value="{{ session('ryokan') }}">
-        <input type="hidden" id="end" value="{{ session('onsen') }}">
+        
+        <input type="hidden" name="ryokan-lat" value="{{ session('ryokan.lat') }}">
+        <input type="hidden" name="ryokan-lng" value="{{ session('ryokan.lng') }}">
+        <input type="hidden" name="onsen-lat" value="{{ session('onsen.lat') }}">
+        <input type="hidden" name="onsen-lng" value="{{ session('onsen.lng') }}">
     </div>
 @endsection
 
