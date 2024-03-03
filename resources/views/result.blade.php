d67c730c92b367fedcb9f50a2775d0eda044376a
ulxsth <ulxsth@gmail.com>
2024-03-04 02:30:43 +0900

    fix: idをnameに指定してた

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -4,11 +4,11 @@
     <div class="container">
         <p>所要時間：<span id="duration"></span></p>
         <div id="map" style="width: 100%; height: 100vh"></div>
-        
-        <input type="hidden" name="ryokan-lat" value="{{ session('ryokan.lat') }}">
-        <input type="hidden" name="ryokan-lng" value="{{ session('ryokan.lng') }}">
-        <input type="hidden" name="onsen-lat" value="{{ session('onsen.lat') }}">
-        <input type="hidden" name="onsen-lng" value="{{ session('onsen.lng') }}">
+
+        <input type="hidden" id="ryokan-lat" value="{{ session('ryokan.lat') }}">
+        <input type="hidden" id="ryokan-lng" value="{{ session('ryokan.lng') }}">
+        <input type="hidden" id="onsen-lat" value="{{ session('onsen.lat') }}">
+        <input type="hidden" id="onsen-lng" value="{{ session('onsen.lng') }}">
     </div>
 @endsection
 
