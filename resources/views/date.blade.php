316aa99678f2f037e0a838631102a94cc1f27903
naoyuki <kamaboko912@gmail.com>
2024-03-05 22:38:26 +0900

    upd: form要素にid追加

--- resources/views/date.blade.php
+++ resources/views/date.blade.php
@@ -2,7 +2,7 @@
 
 @section("content")
 <main class="main">
-    <form action="/date" method="post">
+    <form action="/date" method="post" id="nav-submit">
         @csrf
         <input type="date" name="date" id="date" class="search-input">
     </form>
