5929fff1e811c7f3930e76eb2a47f8217b35c9a9
naoyuki <kamaboko912@gmail.com>
2024-03-05 11:30:44 +0900

    del: labelとsubmitボタンを削除

--- resources/views/date.blade.php
+++ resources/views/date.blade.php
@@ -4,9 +4,7 @@
 <main class="main">
     <form action="/date" method="post">
         @csrf
-        <label for="date">日付</label>
         <input type="date" name="date" id="date" class="search-input">
-        <button type="submit">next</button>
     </form>
 </main>
 @endsection
