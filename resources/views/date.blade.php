d5332f36fda385051e91da51436919129b84dc46
naoyuki <kamaboko912@gmail.com>
2024-03-05 11:27:48 +0900

    upd: mainタグでform内包

--- resources/views/date.blade.php
+++ resources/views/date.blade.php
@@ -1,10 +1,12 @@
 @extends("app")
 
 @section("content")
-<form action="/date" method="post">
-    @csrf
-    <label for="date">日付</label>
-    <input type="date" name="date" id="date">
-    <button type="submit">next</button>
-</form>
+<main class="main">
+    <form action="/date" method="post">
+        @csrf
+        <label for="date">日付</label>
+        <input type="date" name="date" id="date">
+        <button type="submit">next</button>
+    </form>
+</main>
 @endsection
