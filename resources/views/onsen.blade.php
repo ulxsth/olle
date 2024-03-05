a3b20146c3a33b5591b85830bf04efaf37e52f6b
naoyuki <kamaboko912@gmail.com>
2024-03-05 11:03:59 +0900

    upd: 従来のボタン削除し、mainタグでフォームを内包

--- resources/views/onsen.blade.php
+++ resources/views/onsen.blade.php
@@ -2,15 +2,16 @@
 
 
 @section('content')
-    <form action="/onsen" method="post">
+<main class="main">
+    <form action="/onsen" method="post" id="nav-submit">
         @csrf
         <x-auto-complete-input name="onsen" placeholder="温泉名を入力" />
-        <button type="submit">next</button>
-
+        
         {{-- TODO: nameのみにしてjs側からid参照をなくす --}}
         <input type="hidden" id="pac-lat" name="pac-lat">
         <input type="hidden" id="pac-lng" name="pac-lng">
     </form>
+</main>
 @endsection
 
 @section('script')
