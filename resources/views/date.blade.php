dd7d890e4c76f9dfd3bb59ebfdce0b5c1a390f1d
naoyuki <kamaboko912@gmail.com>
2024-03-05 11:30:03 +0900

    upd: input要素にsearch-inputのクラス付与

--- resources/views/date.blade.php
+++ resources/views/date.blade.php
@@ -5,7 +5,7 @@
     <form action="/date" method="post">
         @csrf
         <label for="date">日付</label>
-        <input type="date" name="date" id="date">
+        <input type="date" name="date" id="date" class="search-input">
         <button type="submit">next</button>
     </form>
 </main>
