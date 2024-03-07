87d2c43ef35543bec2d84454e25257fa744513ee
naoyuki <kamaboko912@gmail.com>
2024-03-07 15:52:25 +0900

    upd: 追加ボタンと削除ボタンで、flagの増減をした。

---
+++ public/js/add-flag.js
@@ -0,0 +1,38 @@
+const addButton = document.getElementById("add-flag");
+let deleteButtons = [];
+
+let count = 1;
+addButton.addEventListener("click", (event) => {
+    let inputValue = document.getElementById("pac-input").value;
+    event.preventDefault();
+    const flagsLength = document.querySelectorAll(".flag").length;
+    const flagHeader = `第${flagsLength}通過地点`;
+    const addFlag = `
+        <div class="flag">
+            <div class="flag__header">
+                <h3 class="flag__checkpoint">${flagHeader}</h3>
+                <button class="flag__delete-btn" id="delete-btn-${count}" >削除</button>
+            </div>
+            <p class="flag__name">${inputValue}</p>
+        </div>
+    `;
+
+    count++;
+
+    const flagsWrap = document.querySelector(".flags-wrap");
+    flagsWrap.innerHTML += addFlag;
+    document.getElementById("pac-input").value = "";
+
+    deleteButtons = document.querySelectorAll(".flag__delete-btn");
+
+    deleteButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            event.preventDefault();
+            const deleteButton = event.target;
+            const flagElement = deleteButton.closest(".flag");
+            if (flagElement) {
+                flagElement.remove();
+            }
+        });
+    });
+});
