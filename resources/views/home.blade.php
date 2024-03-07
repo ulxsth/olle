27677c195500b7b70b17afc4d17adefb728c19ec
ulxsth <ulxsth@gmail.com>
2024-03-07 16:58:41 +0900

    upd: footerを追加

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/home.css">
+  <title>olle / オルレ -「さんぽ」を手助けするwebサービス-</title>
+</head>
+
+<body>
+  <header></header>
+  <main>
+    <div class="keyvisual">
+      
+    </div>
+    <div class="description"></div>
+    <div class="howto"></div>
+    <div class="image-slide"></div>
+  </main>
+  <footer>©2024 Gabaithon Team 5</footer>
+</body>
+
+</html>
