2c4f2511b5823cdac02d63674e1db8d7101ba145
ulxsth <ulxsth@gmail.com>
2024-03-07 19:51:34 +0900

    upd: キービジュアルを追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,15 +14,12 @@
     <div class="keyvisual"></div>
     <div class="description">
       <h2 class="description__title">"olle / オルレ" とは？</h2>
-      <p>
+      <p class="description__text">
         旅先での醍醐味、散策。<br>
         "olle / オルレ" は、あなたが寄りたい場所からルートを計算し、あなたに提案してくれます。
         あなただけのオルレコースを作成しましょう。
         <a href="/start" class="btn__start">さっそく使ってみる</a>
       </p>
-    </div>
-    <div class="howto">
-      <a href="/start" class="btn__start">さっそく使ってみる</a>
     </div>
   </main>
   <footer>©2024 Gabaithon Spring Team 5</footer>
