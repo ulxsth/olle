5d6feb825b6bc68dac6ab4f4e5442957782c21d7
naoyuki <kamaboko912@gmail.com>
2024-03-07 17:24:10 +0900

    upd: 最後に追加した中間地点のラベルを「到着地点」にした。それ以外は第n通過地点にした。

--- public/js/add-flag.js
+++ public/js/add-flag.js
@@ -14,11 +14,10 @@ addButton.addEventListener("click", (event) => {
         alert("通過地点を入力してください");
         return;
     }
-    const flagHeader = `第${flagsLength}通過地点`;
     const addFlag = `
         <div class="flag">
             <div class="flag__header">
-                <h3 class="flag__checkpoint">${flagHeader}</h3>
+                <h3 class="flag__checkpoint">到着地点</h3>
                 <button class="flag__delete-btn" id="delete-btn-${count}" >削除</button>
             </div>
             <p class="flag__name">${inputValue}</p>
@@ -30,6 +29,18 @@ addButton.addEventListener("click", (event) => {
     const flagsWrap = document.querySelector(".flags-wrap");
     flagsWrap.innerHTML += addFlag;
     document.getElementById("pac-input").value = "";
+
+    const flags = document.querySelectorAll(".flag");
+    flags.forEach((flag, index) => {
+        const checkpoint = flag.querySelector(".flag__checkpoint");
+        if (index === 0) {
+            checkpoint.textContent = "出発地点";
+        } else if (index < flags.length - 1) {
+            checkpoint.textContent = `第${index}通過地点`;
+        } else {
+            checkpoint.textContent = "到着地点";
+        }
+    });
 
     deleteButtons = document.querySelectorAll(".flag__delete-btn");
 
