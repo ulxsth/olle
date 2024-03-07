7e24b557e36128bdb27480eecc1a0dee2b527d40
kazu <2201009@s.asojuku.ac.jp>
2024-03-08 08:26:32 +0900

    ルート一覧の表示

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
     <div class="container">
+        <!-- ルート案内テキスト表示用の要素 -->
+        <div id="route-instructions" style="display: none;"></div>
+        
+        <button id="toggle-list-button">ルート案内一覧を表示</button>
+        <div id="route-list" style="display: none;">
+            <h3>ルート案内一覧</h3>
+            <ol id="route-steps"></ol>
+        </div>
         <p>所要時間：<span id="duration"></span></p>
         <input checked type="radio" name="travel-mode" value="WALKING">🏃
         <input type="radio" name="travel-mode" value="BICYCLING">🚲
